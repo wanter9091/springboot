6139fa0a28c29c15e3394b91f360b1f0f43499a0
obj <dhqudwo2976@naver.com>
2024-02-06 14:12:31 +0900

    first commit

---
+++ src/main/kotlin/blog/of/obj/BlogApplication.kt
@@ -0,0 +1,11 @@
+package blog.of.obj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlogApplication>(*args)
+}
---
+++ src/test/kotlin/blog/of/obj/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package blog.of.obj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
