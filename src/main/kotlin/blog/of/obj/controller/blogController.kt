9f938eb0e895bc85f8829e129cbb726b46f8af2b
obj <dhqudwo2976@naver.com>
2024-02-07 08:42:36 +0900

    컨트롤러에 서비스 주입하여 서비스 레이어 호출하기

--- src/main/kotlin/blog/of/obj/controller/blogController.kt
+++ src/main/kotlin/blog/of/obj/controller/blogController.kt
@@ -1,35 +1,41 @@
 package blog.of.obj.controller
 
 import org.springframework.web.bind.annotation.*;
+import blog.of.obj.service.blogService
+import blog.of.obj.service.Post
+
+data class CreatePostReqDto(
+    val title: String,
+    val body: String
+)
 
 @RestController
 @RequestMapping("/bolg")
-class BlogMapping{
+class BlogController(val blogService: blogService){
     @GetMapping("/{idx}")
-    fun readOnePost(@PathVariable("idx") idx:Int ):Int{
+    fun readOnePost(@PathVariable("idx") idx:Int, @RequestParam("page", required= false, defaultValue = 0.toString()) page: Int ): Post{
         val list: List<Int> = listOf(0,1,2)
-        return list[0]
+        return blogService.readOnePost(idx)
     }
 
     @GetMapping()
-    fun readAllPost(): List<Int>{
-        val list: List<Int> = listOf(0,1,2)
-        return list
+    fun readAllPost(): List<Post>{
+        return blogService.readAllPost()
     }
 
     @PostMapping()
-    fun createPost(): Boolean{
-        return true
+    fun createPost(@RequestBody() createPostReqDto: CreatePostReqDto): Boolean{
+        return blogService.createPost(createPostReqDto.title, createPostReqDto.body)
     }
 
     @PutMapping("/{idx}")
-    fun modifyPost(@PathVariable("idx") idx:Int ): Boolean{
-        return true
+    fun modifyPost(@PathVariable("idx") idx:Int, @RequestBody() createPostReqDto: CreatePostReqDto ): Boolean{
+        return blogService.modifyPost(idx, createPostReqDto.title, createPostReqDto.body)
     }
 
     @DeleteMapping("/{idx}")
     fun deletePost(@PathVariable("idx") idx:Int ): Boolean{
-        return true
+        return blogService.deletePost(idx)
     }
 
 
