d825b8097954b49c2f98205721b90a36a6b5b4d6
obj <dhqudwo2976@naver.com>
2024-02-07 09:58:40 +0900

    익셉션 처리를 위한 핸들러

--- src/main/kotlin/blog/of/obj/exception/commonException.kt
+++ src/main/kotlin/blog/of/obj/exception/commonException.kt
@@ -1,2 +1,14 @@
 package blog.of.obj.exception
 
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class BaseFilter{
+    @ExceptionHandler(*[java.lang.NullPointerException::class, ClassCastException::class, BaseException::class, java.lang.Exception::class])
+    fun handle(ex: BaseException): ResponseEntity<BaseResponseError>{
+        val errorResponse=BaseResponseError(ex.baseResponseCode, ex.baseResponseCode.massage)
+        return ResponseEntity.status(ex. baseResponseCode.status).body(errorResponse)
+    }
+}
